038ceab1d5ab694435d830fe0bb981c94ea4c5d6
vitham <anhxtanhwp@gmail.com>
2018-08-29 10:19:39 +0700

    Create sign in activity xml

--- app/src/main/java/com/vit/customerapp/ui/feature/main/SignInActivity.java
+++ app/src/main/java/com/vit/customerapp/ui/feature/main/SignInActivity.java
@@ -1,18 +1,15 @@
 package com.vit.customerapp.ui.feature.main;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.vit.customerapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(this, SignInActivity.class));
+        setContentView(R.layout.activity_sign_in);
     }
 }
